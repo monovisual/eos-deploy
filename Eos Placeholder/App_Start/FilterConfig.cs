61d3c5312dbca0a063ca1c4f0602c17e726d4712
Clyde Kent <monovisual@proton.eos.london>
2016-12-10 17:15:12 +0000

    Initial

---
+++ Eos Placeholder/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Eos_Placeholder
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
+        }
+    }
+}
---
+++ Eos Placeholder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eos_Placeholder.Startup))]
+namespace Eos_Placeholder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
